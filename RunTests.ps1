f8ea796b546a81b2e3af34d9ab1ec76596bfab95
Richard.Hannah <Richard.Hannah@solarwinds.com>
2018-09-03 16:15:09 +0100

    add runsettings file

--- RunTests.ps1
+++ RunTests.ps1
@@ -27,4 +27,15 @@ MSTest /testcontainer:TestMountebankWorkshop\bin\debug\TestMountebankWorkshop.dl
 echo "=========================="
 echo "Annotated Integration tests"
 echo "=========================="
+
 MSTest /testcontainer:TestMountebankWorkshop\bin\debug\TestMountebankWorkshop.dll /category:"Integration"
+
+
+echo "=========================="
+echo "Both together"
+echo "Note this doesnt execute any tests as it is looking in the integrationTest namespace for methods tasked with testCategory - of which there are none"
+echo "=========================="
+
+MSTest /testcontainer:TestMountebankWorkshop\bin\debug\TestMountebankWorkshop.dll /test:TestMounteBankWorkshop.IntegrationTest /category:"Integration"
+
+MSTest /testcontainer:TestMountebankWorkshop\bin\debug\TestMountebankWorkshop.dll /testsettings:TestMountebankWorkshop\bin\debug\unit.runsettings
